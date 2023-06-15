9f0a6a878b2473d546eed0a79fa075f6b0268886
angelobarilari <angelomelo_tdai@hotmail.com>
2023-06-15 17:52:41 -0300

    feat: added login route returning jwt

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("register/", views.CreateUserView.as_view()),
     path("users/", views.ListUsersView.as_view()),
-    # path("login/", views.TokenCreateView.as_view()),
+    path("login/", views.TokenCreateView.as_view()),
     # path('users/<uuid:user_id>/', views.UserFilterByIDView.as_view(),),
 ]
