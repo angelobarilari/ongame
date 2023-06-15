40a8644873edbee8752d05870c26ee4c70cda823
angelobarilari <angelomelo_tdai@hotmail.com>
2023-06-15 17:43:35 -0300

    feat: postgres integration and some routes added

---
+++ backend/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("register/", views.CreateUserView.as_view()),
+    path("users/", views.ListUsersView.as_view()),
+    # path("login/", views.TokenCreateView.as_view()),
+    # path('users/<uuid:user_id>/', views.UserFilterByIDView.as_view(),),
+]
