c73182d6bac3f9697b99aacf457a7da52696b39d
angelobarilari <angelomelo_tdai@hotmail.com>
2023-06-15 11:51:55 -0300

    feat: created user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework import serializers
-from .models import User
+from .models import GenderChoices, User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        exclude = '__all__'
+    
+    id = serializers.UUIDField(read_only=True)
+    email = serializers.CharField(unique=True, max_length=255)
+    password = serializers.CharField(write_only=True)
+    name = serializers.CharField(max_length=100)
+    surname = serializers.CharField(max_length=100)
+    gender = serializers.ChoiceField(
+        choices = GenderChoices.choices,
+        default = GenderChoices.PREFER_NOT_SAY
+    )
 
+    birthdate = serializers.DateTimeField()
+
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact = value).exists():
+            raise serializers.ValidationError("email already exists")
+
+        return value
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
 
