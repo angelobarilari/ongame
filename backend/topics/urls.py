39dfed5daffecc491e889787ecc6f4cbcfde1d54
angelobarilari <angelomelo_tdai@hotmail.com>
2023-06-18 18:16:36 -0300

    feat: implented docker backend

--- backend/topics/urls.py
+++ backend/topics/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("topics/", views.ListCreateTopicView.as_view()),
-    path("topics/<int:pk>", views.DetailTopicView.as_view()),
+    path("topics/<int:pk>/", views.DetailTopicView.as_view()),
 ]
