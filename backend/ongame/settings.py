a5dd9919734cc09eb2b8a83f43c268ef4137b4cf
angelobarilari <angelomelo_tdai@hotmail.com>
2023-07-01 11:22:49 -0300

    fix: .env moved to root and env_file is now referenced in backend compose

--- backend/ongame/settings.py
+++ backend/ongame/settings.py
@@ -12,13 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import dotenv
+from dotenv import load_dotenv
 
-dotenv.load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
